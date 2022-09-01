4f7b5c639f5792069ac7ec32653e3cb57af8b086
NabizaBatool <nabizarizvi1@gmail.com>
2022-09-01 11:58:44 +0500

    updates requ

--- todoSite/settings.py
+++ todoSite/settings.py
@@ -138,7 +138,7 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='nabizabatool1@gmail.com'
-EMAIL_HOST_PASSWORD=''
+EMAIL_HOST_PASSWORD='vqbnvvrdztdojteu'
 DEFAULT_FROM_EMAIL='celery <nabizabatool1@gmail.com>'
 
 #Acivate Django heroku
